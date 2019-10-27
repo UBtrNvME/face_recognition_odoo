385ba79cf1d431835c0ce78e1a7d9504e5d985a7
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2019-10-28 00:18:50 +0600

    [UPDATE] Project now on RU, added all fields and defaults

--- __manifest__.py
+++ __manifest__.py
@@ -27,7 +27,7 @@
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
-        # 'views/templates.xml',
+        'views/templates.xml',
         'views/project_project_views.xml',
     ],
     'sequence': -1,
--- models/ProjectProject.py
+++ models/ProjectProject.py
@@ -6,4 +6,23 @@ from odoo import models, fields, api
 class ProjectProject(models.Model):
     _inherit = 'project.project'
 
-    stage_id = fields.Many2one(comodel_name="project.task.type")
+    @api.one
+    @api.depends("partner_id")
+    def _get_creator_email(self):
+        self.creator_email = self.partner_id.email
+
+    @api.one
+    def send_approval(self):
+        template = self.env.ref('gpodem.approval_email_template')
+        self.env['mail.template'].browse(template.id).send_mail(self.id)
+
+    @api.model
+    def get_default_partner(self):
+        return self.env['res.users'].browse(self.env.uid).partner_id
+
+    stage_id = fields.Many2one(comodel_name="project.task.type", string='Стадия')
+    manager_id = fields.Many2one(comodel_name="res.partner", string="Менеджер", default=get_default_partner)
+    description = fields.Text(string='Описание проекта')
+    creator_email = fields.Char(string='Email создателя', compute="_get_creator_email")
+    partner_id = fields.Many2one(comodel_name="res.partner",
+                                 default=get_default_partner)
