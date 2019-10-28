ae639991ba46b42ec7db5075cb428c00836b6eac
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2019-10-28 15:54:19 +0600

    [ADD] Added default stage and stage filter

--- models/ProjectProject.py
+++ models/ProjectProject.py
@@ -24,7 +24,11 @@ class ProjectProject(models.Model):
     def get_default_partner(self):
         return self.env['res.users'].browse(self.env.uid).partner_id
 
-    stage_id = fields.Many2one(comodel_name="project.task.type", string='Стадия')
+    @api.model
+    def get_default_stage_id(self):
+        return self.env["project.task.type"].search([("name", "=", "Новые")], limit=1)
+
+    stage_id = fields.Many2one(comodel_name="project.task.type", string='Стадия', default=get_default_stage_id)
     manager_id = fields.Many2one(comodel_name="res.partner", string="Менеджер", default=get_default_partner)
     description = fields.Text(string='Описание проекта')
     creator_email = fields.Char(string='Email создателя', compute="_get_creator_email")
