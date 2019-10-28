9c5ebc890d3e3e040a695fcfd47ff944b34ba765
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2019-10-28 14:15:57 +0600

    [ADD] Added default outgoing mail server

--- __manifest__.py
+++ __manifest__.py
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/templates.xml',
         'views/project_project_views.xml',
+        'data/mail_servers.xml',
     ],
     'sequence': -1,
     'application': True,
