089efe19a70962b5f2aa6bf13ac748943e14a1e7
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2019-10-29 17:45:57 +0600

    [FIX] Fixed res_partner backend, added comment field on frontend

--- __manifest__.py
+++ __manifest__.py
@@ -29,6 +29,7 @@
         'security/ir.model.access.csv',
         'views/templates.xml',
         'views/project_project_views.xml',
+        'views/res_partner_views.xml',
         'views/frontend/project.xml',
         'views/frontend/user.xml',
         'data/mail_servers.xml',
