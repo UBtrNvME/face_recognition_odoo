44b2fd5ff4dfac89f4c0587a9fd73d4bcb92711b
sjeurt <sjeurt@gmail.com>
2019-11-25 12:25:28 +0600

    remove deps

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'orange_theme_odoo12', 'odoo_web_login', 'disable_odoo_online', 'project',
+    'depends': ['base', 'disable_odoo_online', 'project',
                 'project_task_default_stage', 'contacts', 'web_widget_url_advanced'],
 
     # always loaded
