16c866151c64695b09b00da744af390a6de3696f
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-01-08 02:03:58 +0600

    [ADD] Added initial

--- __manifest__.py
+++ __manifest__.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "gpodem",
+    'name': "Face Recognition",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Face Recognition for Users""",
 
     'description': """
-        Long description of module's purpose
+        Odoo12 module for testing Face Recognition using Python module face-recognition
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "QZHub",
+    'website': "http://qzhub.kz:8080",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -20,22 +19,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'disable_odoo_online', 'project',
-                'project_task_default_stage', 'contacts', 'web_widget_url_advanced'],
+    'depends': ['base', 'contacts'],
 
     # always loaded
     'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/templates.xml',
-        'views/project_project_views.xml',
-        'views/project_region_views.xml',
         'views/res_partner_views.xml',
-        'views/frontend/project.xml',
         'views/frontend/user.xml',
-        'data/mail_servers.xml',
-        'data/project_type_data.xml',
-        'views/resources.xml',
     ],
     'sequence': -1,
     'application': True,
