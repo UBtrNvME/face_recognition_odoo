3391423ecdad7384ec98d5abb70ff3f90360579a
Adi Sabyrbayev <sabyrbayev.adi32@gmail.com>
2020-01-08 02:01:49 +0600

    [ADD] Added initial

--- controllers/project.py
+++ controllers/project.py
@@ -14,7 +14,6 @@ class Project(http.Controller):
     @http.route('/project/<int:project_id>', auth='public')
     def project(self, project_id, **kw):
         project = request.env["project.project"].sudo().search([("id", "=", project_id)])
-        print(project.id)
         return request.render("gpodem.frontend_project", {
             "project": project
         })
