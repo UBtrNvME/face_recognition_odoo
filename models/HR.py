ab32b08e6fb469811f81b2d5b85c8a8fe016411e
UBtrNvME <mooniron.k@gmail.com>
2020-06-30 19:31:55 +0600

    [UPD] Updated face recognition to camera apture

--- models/HR.py
+++ models/HR.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-import cv2, base64, io
-from PIL import Image
+from odoo import fields, models
+
 
 class HR(models.Model):
     _inherit = 'hr.employee'
@@ -10,52 +9,15 @@ class HR(models.Model):
     attachment_ids = fields.Many2many('ir.attachment', 'face_recognition_hr_employee_ir_attachments_rel', 'employee_id',
                                       'attachment_id', string='Photos')
 
-    def method_webcam_trigger(self):
-        key = cv2.waitKey(1)
-        webcam = cv2.VideoCapture(0)
-        while True:
-            try:
-                print("4")
-                check, frame = webcam.read()
-                print(webcam.isOpened(), check, frame)
-                if check:
-                    cv2.imshow("Capturing", frame)
-                    print("*********")
-                    key = cv2.waitKey(1)
-                    if key == ord('s'):
-                        print("SAVE IMAGE")
-                        cv2.imwrite(filename='product_image', img=frame)
-                        img = Image.fromarray(frame, 'RGB')
-                        img.save('my.jpg')
-                        roiImg = img.crop()
-                        imgByteArr = io.BytesIO()
-                        roiImg.save(imgByteArr, format='JPG')
-                        imgByteArr = imgByteArr.getvalue()
-                        image_base64 = base64.b64encode(imgByteArr)
-                        cv2.waitKey(1650)
-                        cv2.destroyAllWindows()
-                        vals = {
-                            'image': image_base64,
-                        }
-                        self.write(vals)
-                        break
-                    elif key == ord('q'):
-                        break
-            except(KeyboardInterrupt):
-                webcam.release()
-                cv2.destroyAllWindows()
-                break
-        webcam.release()
-        cv2.destroyAllWindows()
-
     def compare_with_image(self):
         return {
             'res_model': 'face.recognition',
-            'type': 'ir.actions.act_window',
-            'context': {
-                "default_employee_id": self.id
+            'type'     : 'ir.actions.act_window',
+            'context'  : {
+                "default_employee_id"      : self.id,
+                "default_loaded_image"     : self.env['face.recognition'].camera_capture()
             },
             'view_mode': 'form',
             # 'view_id': self.env.ref("face_recognition.face_recognition_compare_form").id,
-            'target': 'new'
+            'target'   : 'new'
         }
