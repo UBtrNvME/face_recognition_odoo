0c2cd4397ff20d89f0b363199e8adf7aedb0794e
UBtrNvME <mooniron.k@gmail.com>
2020-09-21 13:17:30 +0600

    [ADD] Add uid recognition to the registration page with pool

--- fr_core/models/UinRecognition.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class UinRecognition(models.Model):
-    _name = 'uin.recognition'
-
-    name = fields.Char(string='Name')
-    image = fields.Binary(string='UIN image')
