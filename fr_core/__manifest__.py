9d60f35d974be658236df64c3c61468c2823f307
UBtrNvME <mooniron.k@gmail.com>
2020-09-25 16:01:59 +0600

    [FIX] Fix problem with ellipse not being shown on the video tag on google chrome

--- fr_core/__manifest__.py
+++ fr_core/__manifest__.py
@@ -19,7 +19,7 @@
     'version'    : '0.1',
 
     # any module necessary for this one to work correctly
-    'depends'    : ['base', 'contacts', 'hr', 'hr_attendance', 'web'],
+    'depends'    : ['base', 'contacts', 'hr', 'hr_attendance', 'web', 'website'],
 
     "external_dependencies": {"python": [
         'pytesseract',
