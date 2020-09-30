d3ff884e6d4a042acc6e4407ebf3e84d99086f71
UBtrNvME <mooniron.k@gmail.com>
2020-09-30 11:52:24 +0600

    [UPD] Update of the registration page1
    [UPD] 2

--- fr_core/models/ResPartner.py
+++ fr_core/models/ResPartner.py
@@ -12,3 +12,11 @@ class ResPartner(models.Model):
     face_model_id = fields.Many2one(comodel_name="res.partner.face.model",
                                     string="Face Encodings")
     iin = fields.Char(string="Individual Identification Number")
+    dob = fields.Date(string="Date of Birth")
+    nationality = fields.Char(string='Nationality')
+    gender = fields.Selection(
+        string='Gender',
+        default='un_chosen',
+        selection=[('male', 'Male'), ('female', 'Female'), ('un_chosen', 'Un chosen')]
+    )
+
