df9bcb248eb0149e44f03bd71bf051ba729ad962
UBtrNvME <mooniron.k@gmail.com>
2020-12-06 21:42:56 +0600

    [UPD] Updated cad.symbol form pallete
    Signed-off-by: UBtrNvME <mooniron.k@gmail.com>

--- cad_automation/__manifest__.py
+++ cad_automation/__manifest__.py
@@ -14,7 +14,7 @@
     "version": "13.0.1.0.0",
     # any module necessary for this one to work correctly
     "depends": ["base", "web"],
-    "external_dependencies": {"python": ["cv2"], "bin": []},
+    "external_dependencies": {"python": ["cv2", "numpy"], "bin": []},
     # always loaded
     "data": ["security/ir.model.access.csv", "views/views.xml", "views/templates.xml"],
     # only loaded in demonstration mode
--- cad_automation/models/models.py
+++ cad_automation/models/models.py
@@ -4,6 +4,7 @@ import logging
 from odoo import api, fields, models
 
 import cv2
+import numpy as np
 
 from ..services.img_manipulation import base64_to_ndarray, ndarray_to_base64
 
@@ -12,6 +13,8 @@ _logger = logging.getLogger(__name__)
 COLOR_RED = (0, 0, 255)
 COLOR_BLUE = (255, 0, 0)
 COLOR_GREEN = (0, 255, 0)
+MASK_THROUGH = 210
+MASK_BLOCK = 0
 
 
 class CadSymbol(models.Model):
@@ -19,34 +22,90 @@ class CadSymbol(models.Model):
 
     name = fields.Char(string="Name of the Cad Object Template", required=True)
     preview = fields.Binary(string="Preview", compute="_compute_preview")
-    template = fields.Image(string="Image representation of the Cad Object")
-    mask = fields.Image(string="Mask of the Cad Object Template", readonly=True)
-    threshold = fields.Integer(string="Threshold")
+    template = fields.Binary(string="Image representation of the Cad Object")
+    template_b64 = fields.Text(string="base64")
+    mask = fields.Binary(string="Mask of the Cad Object Template", readonly=True)
+    threshold = fields.Float(string="Threshold")
     origin = fields.Char(string="Origin")
     connections = fields.Text(string="Json Repr of Connections")
     width = fields.Integer(string="Width of the Template")
     height = fields.Integer(string="Height of the Template")
+    ignore_regions = fields.Text(string="Region to Mask")
 
-    @staticmethod
-    def generate_mask(image):
-        image_b64 = image
-        ndarray = base64_to_ndarray(image_b64)
-        return ndarray_to_base64(
-            cv2.threshold(ndarray, 100, 250, cv2.THRESH_BINARY_INV)[1]
-        )
+    @api.onchange("template")
+    def onchange_template(self):
+        for rec in self:
+            rec.template_b64 = rec.template
 
     @api.model
     def create(self, vals):
-        if vals.get("image_representation"):
-            vals["mask"] = self.generate_mask(image=vals["image_representation"])
+        if template := vals.get("template"):
+            vals.update(self._initialize_template(template))
+
+        ignore = vals.get("ignore_regions")
+        if ignore and template:
+            mask = self._generate_mask(template, json.loads(ignore))
+            vals["mask"] = ndarray_to_base64(mask)
+
         return super().create(vals)
 
-    @api.depends("template", "connection", "origin")
+    @api.depends("template_b64", "connections", "ignore_regions")
     def _compute_preview(self):
-        ndarray = base64_to_ndarray(self.template)
-        origin = json.loads(self.origin)
-        connections = json.loads(self.connections)
-        cv2.circle(ndarray, origin, 2, COLOR_RED, -1)
-        for connection in connections:
-            cv2.circle(ndarray, connection["pos"], 2, COLOR_GREEN, -1)
-        self.preview = ndarray
+        for rec in self:
+            if rec.template_b64:
+                ndarray = base64_to_ndarray(rec.template_b64)
+
+                if rec.ignore_regions:
+                    mask = self._generate_mask(
+                        rec.template_b64, json.loads(rec.ignore_regions)
+                    )
+                    ndarray = np.array(
+                        np.where(mask == MASK_THROUGH, ndarray, (0, 255, 0)),
+                        dtype=np.uint8,
+                    )
+
+                if rec.origin:
+                    origin = json.loads(rec.origin)
+                    cv2.circle(ndarray, tuple(origin), 2, COLOR_RED, -1)
+
+                if rec.connections:
+                    connections = json.loads(rec.connections)
+
+                    for connection in connections:
+                        cv2.circle(
+                            ndarray, tuple(connection["pos"]), 2, COLOR_GREEN, -1
+                        )
+
+                rec.preview = ndarray_to_base64(ndarray)
+            else:
+                rec.preview = None
+
+    @staticmethod
+    def _initialize_template(template):
+        shape = base64_to_ndarray(template).shape
+        return {
+            "template_b64": template,
+            "width": shape[1],
+            "height": shape[0],
+            "origin": f"[{shape[1] // 2},{shape[0] // 2}]",
+        }
+
+    @staticmethod
+    def _generate_mask(template, ignore):
+        template = base64_to_ndarray(template)
+
+        mask = np.full(template.shape, MASK_THROUGH, dtype=np.uint8)
+        for contour in ignore:
+            cv2.fillPoly(mask, pts=[np.array(contour)], color=MASK_BLOCK)
+        return mask
+
+    def write(self, vals):
+        if template := vals.get("template"):
+            vals.update(self._initialize_template(template))
+
+        ignore = vals.get("ignore_regions")
+        if ignore and self.template_b64:
+            mask = self._generate_mask(self.template_b64, json.loads(ignore))
+            vals["mask"] = ndarray_to_base64(mask)
+
+        return super().write(vals)
